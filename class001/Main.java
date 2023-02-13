a161f5d07c1e546b2714be305e7f0474e832bfd3
surikunal <suri.kunal.222@gmail.com>
2023-02-13 22:42:00 +0530

    Update

---
+++ class001/Main.java
@@ -0,0 +1,56 @@
+public class Main {
+    public static void main(String[] args) {
+        // data type , name the variable, constant value
+        // int a = 5;
+        // System.out.println(a);  // variable name
+
+        // a = 6;
+        // System.out.println(a);
+
+        // String b = "ABC";
+        // System.out.println(b);
+
+        // b = "XYZ";
+        // System.out.println(b);
+
+        // boolean c = true;
+        // System.out.println(c);
+
+        // char d = 'x';
+        // System.out.println(d);
+
+        // =================================================
+
+        // int a = 2;
+        // int b = 3;
+
+        // int sum = a + b;
+
+        // System.out.println(sum);
+
+        // int minus = a - b;
+        // System.out.println(minus);
+
+        // int mult = a * b;
+        // System.out.println(mult);
+
+        // int div = a / b;
+        // System.out.println(div);
+
+        // =====================================================
+
+        int a = 5;
+        a++;    // post increment
+        System.out.println(a);
+
+        a--;
+        a--;
+        System.out.println(a);
+
+        a = a + 5;  // 10
+        a += 5; // 15
+
+
+
+    }
+}
